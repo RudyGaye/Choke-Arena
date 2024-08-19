a337e9f8f42a319f54ab62b586ff6b8e79d0539c
Rudy <gayerudy@gmail.com>
2024-08-19 13:35:19 +0200

    Modification formulaire inscription

--- ChokeArena/User_Management/templatetags/custom_filters.py
+++ ChokeArena/User_Management/templatetags/custom_filters.py
@@ -1,13 +1,21 @@
 from django import template
+from django.forms.utils import flatatt
 
 register = template.Library()
 
 @register.filter
 def add_class(field, css_class):
     """Ajoute une classe CSS à un champ de formulaire."""
-    if not field.field.widget.attrs:
-        field.field.widget.attrs = {}
-    current_classes = field.field.widget.attrs.get('class', '')
+    if not hasattr(field, 'field') or not field.field.widget.attrs:
+        return field
+
+    # Assurez-vous que le champ est bien un champ de formulaire
+    widget = field.field.widget
+    attrs = widget.attrs.copy()
+    current_classes = attrs.get('class', '')
     new_classes = f'{current_classes} {css_class}'.strip()
-    field.field.widget.attrs['class'] = new_classes
+    attrs['class'] = new_classes
+
+    # Modifiez les attributs du widget et renvoyez le champ
+    widget.attrs = attrs
     return field
