baae2c41a00e824709f475e5f4cf7bdbcb61e02a
Rudy <gayerudy@gmail.com>
2024-08-20 08:26:03 +0200

    Modification reglage

--- ChokeArena/User_Management/models.py
+++ ChokeArena/User_Management/models.py
@@ -3,11 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from Techniques_Library.models import Technique
 from Training_Plans.models import TrainingPlan
-from django.contrib.auth import get_user_model
 
-
-
-User = get_user_model()
 
 class CustomUserManager(BaseUserManager):
     """
--- ChokeArena/User_Management/views.py
+++ ChokeArena/User_Management/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import LoginForm, CustomUserCreationForm, CustomUserChangeForm,
+from .forms import LoginForm, CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 from Techniques_Library.models import Technique
 from Training_Plans.models import TrainingPlan
